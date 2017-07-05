110ca6b8b026cf22fbd8d20b970f33c2d1a651e1
harsh011 <harshbhardwaj2357@gmail.com>
2017-07-05 13:43:21 +0530

    added our_team

--- layout/scripts/jquery.backtotop.js
+++ layout/scripts/jquery.backtotop.js
@@ -1,3 +1,12 @@
+/*
+Template Name: Preevent
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
 jQuery("#backtotop").click(function () {
     jQuery("body,html").animate({
         scrollTop: 0
